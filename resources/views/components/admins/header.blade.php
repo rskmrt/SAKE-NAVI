3ab3db7bbc606979dda5e1dcc35a724677673d68
Murota <ryosuke.murota123@gmail.com>
2022-05-03 20:55:02 +0900

    commit

--- resources/views/components/admins/header.blade.php
+++ resources/views/components/admins/header.blade.php
@@ -1,4 +1,4 @@
-<div class="container d-flex flex-wrap justify-content-center">
+<div class="container">
     <a href="/admin" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
       <svg class="bi me-1" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
       <span class="fs-2">{{ config('app.name', 'Laravel') }}</span>
