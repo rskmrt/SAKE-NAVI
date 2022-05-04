2612477fd9c0ac68cefbf799b0c06fe28bf17bfe
Murota <ryosuke.murota123@gmail.com>
2022-05-05 04:04:47 +0900

    commit

--- resources/views/components/admins/navbar.blade.php
+++ resources/views/components/admins/navbar.blade.php
@@ -6,11 +6,37 @@
     </button>
 
     <div style="overflow: auto">
-      <ul class="nav me-auto">
+      <ul class="nav me-auto" style="width: 14em">
         <li class="nav-item"><a href="/admin" class="nav-link link-dark px-2 active" aria-current="page">カクテル一覧</a></li>
         <li class="nav-item"><a href="/admin/users" class="nav-link link-dark px-2">ユーザ一覧</a></li>
       </ul>
     </div>
+
+    <ul class="nav description" style="margin: 0; margin-left: auto;">
+      @guest
+        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link link-dark px-2">{{ __('Login') }}</a></li>
+        @if (Route::has('register'))
+        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link link-dark px-2">{{ __('Register') }}</a></li>
+        @endif
+      @else
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link link-dark px-2 dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
+  
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+      @endguest 
+    </ul>
     
   </div>
 </nav>
--- resources/views/components/users/navbar.blade.php
+++ resources/views/components/users/navbar.blade.php
@@ -6,13 +6,39 @@
     </button>
 
     <div style="overflow: auto">
-      <ul class="nav me-auto" style="width: 40em">
+      <ul class="nav me-auto" style="width: 30em">
         <li class="nav-item"><a href="/home" class="nav-link link-dark px-2 active" aria-current="page">カクテル一覧</a></li>
         <li class="nav-item"><a href="/favorite" class="nav-link link-dark px-2">お気に入り</a></li>
         <li class="nav-item"><a href="/original" class="nav-link link-dark px-2">オリジナルカクテル</a></li>
         <li class="nav-item"><a href="/can-make" class="nav-link link-dark px-2">作れるカクテル</a></li>
       </ul>
     </div>
+
+    <ul class="nav description" style="margin: 0; margin-left: auto;">
+      @guest
+        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link link-dark px-2">{{ __('Login') }}</a></li>
+        @if (Route::has('register'))
+        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link link-dark px-2">{{ __('Register') }}</a></li>
+        @endif
+      @else
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link link-dark px-2 dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
+  
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+      @endguest 
+    </ul>
 
   </div>
 </nav>
