2147e8d3a09bb38fafb4fb44150b7a5d03448809
Murota <ryosuke.murota123@gmail.com>
2022-05-04 23:32:15 +0900

    commit

--- resources/views/components/users/header.blade.php
+++ resources/views/components/users/header.blade.php
@@ -1,6 +1,38 @@
-<div class="container">
-  <a href="/home" class="mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-    <svg class="bi me-1" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-    <span class="fs-2">{{ config('app.name', 'Laravel') }}</span>
-  </a>
+<div class="container" style="display: flex">
+
+  <div>
+    <a href="/home" class="mb-3 me-lg-auto text-dark text-decoration-none" >
+      <svg class="bi me-1" width="10" height="32"><use xlink:href="#bootstrap"/></svg>
+      <span class="fs-2">{{ config('app.name', 'Laravel') }}</span>
+    </a>
+  </div>
+
+
+    <ul class="nav" style="margin-right: 10%; margin-left: auto;">
+      @guest
+        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link link-dark px-2">{{ __('Login') }}</a></li>
+        @if (Route::has('register'))
+        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link link-dark px-2">{{ __('Register') }}</a></li>
+        @endif
+      @else
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link link-dark px-2 dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+      @endguest 
+    </ul>
+ 
+  
 </div>
+
--- resources/views/components/users/navbar.blade.php
+++ resources/views/components/users/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="py-2 bg-light border-bottom">
-  <div class="container d-flex flex-wrap">
+  <div class="container" style="display: flex">
 
     <div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,32 +16,7 @@
       </ul>
     </div>
 
-    <div>
-      <ul class="nav">
-        @guest
-          <li class="nav-item"><a href="{{ route('login') }}" class="nav-link link-dark px-2">{{ __('Login') }}</a></li>
-          @if (Route::has('register'))
-          <li class="nav-item"><a href="{{ route('register') }}" class="nav-link link-dark px-2">{{ __('Register') }}</a></li>
-          @endif
-        @else
-          <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link link-dark px-2 dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              {{ Auth::user()->name }} <span class="caret"></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-              </a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-              </form>
-            </div>
-          </li>
-        @endguest 
-      </ul>
-    </div>
+    
     
   </div>
 </nav>
